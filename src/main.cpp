26cb6f812d1b55ab91d7f71b365531a386a09da2
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-23 20:47:21 -0700

    setup empty window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,65 @@
 #include <benchmark/benchmark.h>
 #include "include/bs_threadpool.hpp"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+/* initialize GLAD and GLFW, then create window object and set callbacks */
+void createGLcontexts(GLFWwindow*& window) {
+  if (!glfwInit()) {
+    std::cerr << "ERROR::GLFW_INITIALIZATION_FAILURE" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(800, 600, "simulake", nullptr, nullptr);
+  if (nullptr == window) {
+    std::cerr << "ERROR::GLFW_WINDOW_CREATION_FAILURE" << std::endl;
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Set a framebuffer size callback
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "ERROR::GLAD_INITIALIZATION_FAILURE" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  glViewport(0, 0, 800, 600);
+}
+
 int main() {
-  BS::thread_pool tp(10);
+  GLFWwindow* window;
+  createGLcontexts(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
