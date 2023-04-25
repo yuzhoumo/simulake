40f9f4c6d8ebca9b369b0ad9485291045fd6d987
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-25 04:22:01 -0700

    remove unused definitions from constants

--- lib/constants.hpp
+++ lib/constants.hpp
@@ -6,21 +6,6 @@
 #define WINDOW_DEFAULT_WIDTH  800
 #define WINDOW_DEFAULT_HEIGHT 600
 
-/* fullscreen quad (two triangles) for the vertex shader since raymarching is
- * done entirely in fragent shader */
-const float FS_QUAD[] = {
-  -1.0f, -1.0f, 0.0f,
-   1.0f, -1.0f, 0.0f,
-  -1.0f,  1.0f, 0.0f,
-   1.0f,  1.0f, 0.0f
-};
-
-/* define opengl function params for fullscreen quad */
-#define FS_QUAD_VERTEX_ATTRIB_INDEX 0
-#define FS_QUAD_DRAW_ARRAY_PARAMS GL_TRIANGLE_STRIP, 0, 4
-#define FS_QUAD_VERTEX_ATTRIB_PARAMS FS_QUAD_VERTEX_ATTRIB_INDEX, \
-    3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0
-
 /* vertex and fragment shader locations */
 #define VERTEX_SHADER_PATH "./shaders/vertex.glsl"
 #define FRAGMENT_SHADER_PATH "./shaders/fragment.glsl"
